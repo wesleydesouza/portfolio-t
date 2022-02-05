46c88c0fe61e14b3758b4ad23936498062a3d925
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-02-05 12:26:29 -0300

    new post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import GlobalStyle, { Header } from "./styles.js";
-import { Main, Banner, Contats, Card } from "./styles.js";
+import { Main, Banner,Header,Nav, Contats,ContainerGroup, Card, BoxTitulo,Sobre, Container, GroupSobre } from "./styles.js";
+import GlobalStyle from "./styles.js";
 
 function App() {
   return (
@@ -7,29 +7,55 @@ function App() {
       <GlobalStyle />
       <Main>
         <Banner>
-
+        <Contats>
+                <ul>
+                  <li><img src="https://img.icons8.com/ios/30/000000/linkedin.png"/></li>
+                  <li><img src="https://img.icons8.com/ios/30/000000/linkedin.png" alt="instagram"/></li>
+                  <li><img src="https://img.icons8.com/ios/30/000000/linkedin.png" alt="instagram"/></li>
+                </ul>
+          </Contats>
+          <Container>
           <Header>
       
             <h3>Wesley<span>Souza</span></h3>
-            <ul>
-              <li>Home</li>
-              <li>Sobre</li>
-              <li>Conhecimentos</li>
-              <li>Objetivo</li>
-              <li>Contatos</li>
-            </ul>
+            <Nav>
+              <ul>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Conhecimentos</li>
+                <li>Objetivo</li>
+                <li>Contatos</li>
+              </ul>
+            </Nav>
           </Header>
             <Card>
-              <Contats>
-                <ul>
-                  <li><img/></li>
-                  <li><img/></li>
-                  <li><img/></li>
-                </ul>
-              </Contats>
+              
+              <BoxTitulo>
+
                 <h3><span>Hello I'm</span></h3>
+                <h1>Wesley de Souza</h1>
+                <h4>profissional freelance and web developer</h4>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+              </BoxTitulo>
+              <img src="https://s3-sa-east-1.amazonaws.com/doctoralia.com.br/doctor/912957/91295778c2a22a7fd7d91496b859f1e0_large.jpg"/>
           </Card>
+          </Container>
         </Banner>
+        <Sobre>
+          <ContainerGroup>
+            <GroupSobre>
+              <div>
+                <img src="https://s3-sa-east-1.amazonaws.com/doctoralia.com.br/doctor/912957/91295778c2a22a7fd7d91496b859f1e0_large.jpg"/>
+              </div>
+              <BoxTitulo>
+                <h3><span>Sobre</span></h3>
+                      <h1>Perfil Profissional!</h1>
+                      <hr/>
+                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+              </BoxTitulo>
+            </GroupSobre>
+          </ContainerGroup>
+        </Sobre>
       </Main>
     </>
   );
--- src/styles.js
+++ src/styles.js
@@ -6,24 +6,30 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         font-family: sans-serif;
-       
+        
     }
 `;
 
 export const Main = styled.main`
-    padding: 0 5%;
+   
 `;
+/*Section 1*/
+
 
 export const Header = styled.header`
-
+    
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-between;
     width: 100%;
-
+    margin-top: 5px;
+    font-size: 18px;
     span{
         
         color: #900020
     }
+`;
+
+export const Nav = styled.nav`
 
     ul{
         color: #FFF;
@@ -32,25 +38,110 @@ export const Header = styled.header`
     ul li{
         list-style: none;
         margin-left: 15px;
-        
+        font-size: 18px;
     }
 `;
 
 export const Banner = styled.section`
+    display: flex;
     height: 500px;
     background-image: linear-gradient(76deg, white 50%, #900020 40%)
 `;
 
+export const Container = styled.div`
+    margin-left: 5%;
+    width: 70%
+`
 export const Card = styled.div`
-    border: 1px solid red;
-    display:flex
+    
+    display:flex;
+    margin-top: 150px;
+
+    img{
+        margin-top: -60px;
+        width:380px;
+    }
+    
 `;
  
 export const Contats = styled.div`
     display: flex;
     flex-direction: column;
-
+    margin-left:10%;
+    ul{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        height: 180px;
+        padding: 1px;
+        border: 1px solid grey;
+        border-radius: 20px;
+        margin-top: 190px;
+        
+    }
     ul li{
         list-style: none;
     }
- `
+ `;
+
+ export const BoxTitulo = styled.div`
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    width:35%;
+    height: 200px;
+
+    h3 span{
+        color: #900020;
+    };
+
+    h1{
+        font-size: 35px;
+    }
+    h4{
+        font-weight:600;
+        font-size: 17px;
+        margin-top: -10px
+    }
+    p{
+        color: grey;
+        font-size: 15px;
+    }
+    
+    
+ `;
+ /*section 2*/
+
+ export const Sobre = styled.section`
+ 
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    height: 500px;
+ `;
+ 
+export const ContainerGroup = styled.div`
+    width: 60%;
+    
+
+`;
+
+export const GroupSobre = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    
+    h1{
+        font-size: 32px;
+    }
+    hr{
+        border: 2px dashed #900020;
+        border-radius: 4px;
+        width: 25%;
+        
+    }
+    img{
+        width:300px;
+        
+    }
+
+`
